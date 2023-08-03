25fdcc7e6d9faa5ee56fbe609d3150c8478bae6b
Abdeljalil Salhi <abdousalhi868@gmail.com>
2023-08-03 14:43:26 +0100

    Add run scripts from command line

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from srcs.shell import run
+from sys import argv
 
 if __name__ == "__main__":
+    if len(argv) > 1:
+        for i in range(1, len(argv)):
+            with open(argv[i], "r") as f:
+                content = f.read()
+            try:
+                result, error = run(argv[i], content)
+                if error:
+                    print(error.as_string())
+            except Exception as e:
+                print(e.__class__.__name__ + ": " + str(e))
+        exit(0)
     try:
         while True:
             line = input("imenox > ").strip()
